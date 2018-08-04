9f94727a54adb64ca148f2b09760c2b6e6a3413b
何玉龙 <bryantsisu@gmail.com>
2018-08-04 15:55:25 +0800

    init commit

---
+++ 1.桥接模式/BridgePattern/BridgePattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgePattern
+//
+//  Created by 何玉龙 on 2018/8/4.
+//  Copyright © 2018 BryantStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1.桥接模式/BridgePattern/BridgePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgePattern
+//
+//  Created by 何玉龙 on 2018/8/4.
+//  Copyright © 2018 BryantStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
