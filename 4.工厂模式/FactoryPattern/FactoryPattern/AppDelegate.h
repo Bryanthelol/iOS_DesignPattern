5dd379f8cd14611a3133e63ec1df2daf91db7852
何玉龙 <bryantsisu@gmail.com>
2018-08-12 16:29:40 +0800

    hyl

---
+++ 4.工厂模式/FactoryPattern/FactoryPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FactoryPattern
+//
+//  Created by 何玉龙 on 2018/8/12.
+//  Copyright © 2018 BryantStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 4.工厂模式/FactoryPattern/FactoryPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FactoryPattern
+//
+//  Created by 何玉龙 on 2018/8/12.
+//  Copyright © 2018 BryantStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
